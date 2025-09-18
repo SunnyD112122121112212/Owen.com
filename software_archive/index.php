e3126935fe56ce21e4c3525e1ce90560e495b223
JamesRepina <owenlwatts@gmail.com>
2025-09-18 16:27:47 -0500

    software arc added

---
+++ software_archive/index.php
@@ -0,0 +1,17 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Software Archive</title>
+</head>
+<body>
+    <h1>
+        Welcome to Owen's Software Archive.
+    </h1>
+</body>
+</html>
